35d5aa96a81559a1031ce4b5495ff716ccd6bbb1
Jack Cherng <jfcherng@gmail.com>
2020-04-26 03:22:58 +0800

    refactor: plural version of adding buttons
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- src/RoundcubePluginTrait.php
+++ src/RoundcubePluginTrait.php
@@ -17,103 +17,169 @@ trait RoundcubePluginTrait
     abstract public function add_button(array $p, string $container);
 
     /**
-     * Add a button to "attachmentmenu".
+     * Add buttons to the "attachmentmenu" container.
      *
      * @todo refactor this method into another helper class
      *
-     * @param array       $btn  the button
+     * @param array[]     $btns the buttons
      * @param null|string $skin the skin name
      */
-    public function add_button_attachmentmenu(array $btn, ?string $skin = null): void
+    public function add_buttons_attachmentmenu(array $btns, ?string $skin = null): void
     {
         $skin = $skin ?? RoundcubeHelper::getBaseSkinName();
 
-        $btn['_id'] = $btn['_id'] ?? 'WTF_NO_BASE_ID';
-        $btn['class'] = $btn['class'] ?? '';
-        $btn['classact'] = $btn['classact'] ?? '';
-        $btn['innerclass'] = $btn['innerclass'] ?? '';
+        $btns = \array_map(function (array $btn) use ($skin): array {
+            $btn['_id'] = $btn['_id'] ?? 'WTF_NO_BASE_ID';
+            $btn['class'] = $btn['class'] ?? '';
+            $btn['classact'] = $btn['classact'] ?? '';
+            $btn['innerclass'] = $btn['innerclass'] ?? '';
 
-        $btn['type'] = 'link-menuitem';
-        $btn['id'] = "attachmenu{$btn['_id']}";
+            $btn['type'] = 'link-menuitem';
+            $btn['id'] = "attachmenu{$btn['_id']}";
 
-        switch ($skin) {
-            case 'classic':
-                $btn['class'] .= " {$btn['_id']}link";
-                $btn['classact'] .= " {$btn['_id']}link active";
-                $btn['innerclass'] .= " {$btn['_id']}link";
-                break;
-            case 'elastic':
-                $btn['class'] .= " {$btn['_id']} disabled";
-                $btn['classact'] .= " {$btn['_id']} active";
-                break;
-            case 'larry':
-                $btn['class'] .= ' icon';
-                $btn['classact'] .= ' icon active';
-                $btn['innerclass'] .= " icon {$btn['_id']}";
-                break;
-            default:
-                break;
+            switch ($skin) {
+                case 'classic':
+                    $btn['class'] .= " {$btn['_id']}link";
+                    $btn['classact'] .= " {$btn['_id']}link active";
+                    $btn['innerclass'] .= " {$btn['_id']}link";
+                    break;
+                case 'elastic':
+                    $btn['class'] .= " {$btn['_id']} disabled";
+                    $btn['classact'] .= " {$btn['_id']} active";
+                    break;
+                case 'larry':
+                    $btn['class'] .= ' icon';
+                    $btn['classact'] .= ' icon active';
+                    $btn['innerclass'] .= " icon {$btn['_id']}";
+                    break;
+                default:
+                    break;
+            }
+
+            return $btn;
+        }, $btns);
+
+        foreach ($btns as $btn) {
+            $this->add_button($btn, 'attachmentmenu');
         }
-
-        $this->add_button($btn, 'attachmentmenu');
     }
 
     /**
-     * Add a button to "loginfooter".
+     * Add buttons to the "loginfooter" container.
      *
-     * @param array       $btns the buttons
+     * @param array[]     $btns the buttons
      * @param null|string $skin the skin name
      */
-    public function add_button_loginfooter(array $btn, ?string $skin = null): void
+    public function add_buttons_loginfooter(array $btns, ?string $skin = null): void
     {
         $skin = $skin ?? RoundcubeHelper::getBaseSkinName();
 
-        $btn['type'] = 'link';
-        $btn['class'] = $btn['class'] ?? '';
-        $btn['innerclass'] = $btn['innerclass'] ?? '';
-        $btn['badgeType'] = $btn['badgeType'] ?? 'secondary';
+        $btns = \array_map(function (array $btn) use ($skin): array {
+            $btn['type'] = 'link';
+            $btn['class'] = $btn['class'] ?? '';
+            $btn['innerclass'] = $btn['innerclass'] ?? '';
+            $btn['badgeType'] = $btn['badgeType'] ?? 'secondary';
 
-        // should always has 'support-link' class
-        $btn['class'] .= ' support-link';
+            // should always has 'support-link' class
+            $btn['class'] .= ' support-link';
 
-        if ($skin === 'elastic') {
-            $btn['class'] .= " badge badge-{$btn['badgeType']}";
-            $btn['data-toggle'] = $btn['data-toggle'] ?? 'tooltip';
+            if ($skin === 'elastic') {
+                $btn['class'] .= " badge badge-{$btn['badgeType']}";
+                $btn['data-toggle'] = $btn['data-toggle'] ?? 'tooltip';
+            }
+
+            return $btn;
+        }, $btns);
+
+        foreach ($btns as $btn) {
+            $this->add_button($btn, 'loginfooter');
         }
-
-        $this->add_button($btn, 'loginfooter');
     }
 
     /**
-     * Add a button to "taskbar".
+     * Add buttons to the "mailboxoptions" container.
      *
-     * @param array       $btns the buttons
+     * @param array[]     $btns the buttons
      * @param null|string $skin the skin name
      */
-    public function add_button_taskbar(array $btn, ?string $skin = null): void
+    public function add_buttons_mailboxoptions(array $btns, ?string $skin = null): void
     {
         $skin = $skin ?? RoundcubeHelper::getBaseSkinName();
 
-        $btn['type'] = 'link';
-        $btn['class'] = $btn['class'] ?? '';
-        $btn['innerclass'] = $btn['innerclass'] ?? '';
-
-        switch ($skin) {
-            case 'classic':
-                $btn['class'] .= ' button-nthu-ee';
-                break;
-            case 'elastic':
-                $btn['class'] .= ' nthu-ee manual';
-                $btn['innerclass'] .= ' inner';
-                break;
-            case 'larry':
-                $btn['class'] .= ' button-nthu-ee';
-                $btn['innerclass'] .= ' button-inner';
-                break;
-            default:
-                break;
+        foreach ($btns as $btn) {
+            $this->add_button($btn, 'mailboxoptions');
         }
+    }
 
-        $this->add_button($btn, 'taskbar');
+    /**
+     * Add buttons to the "taskbar" container.
+     *
+     * @param array[]     $btns the buttons
+     * @param null|string $skin the skin name
+     */
+    public function add_buttons_taskbar(array $btns, ?string $skin = null): void
+    {
+        $skin = $skin ?? RoundcubeHelper::getBaseSkinName();
+
+        $btns = \array_map(function (array $btn) use ($skin): array {
+            $btn['type'] = 'link';
+            $btn['class'] = $btn['class'] ?? '';
+            $btn['innerclass'] = $btn['innerclass'] ?? '';
+
+            switch ($skin) {
+                case 'classic':
+                    $btn['class'] .= ' button-nthu-ee';
+                    break;
+                case 'elastic':
+                    $btn['class'] .= ' nthu-ee manual';
+                    $btn['innerclass'] .= ' inner';
+                    break;
+                case 'larry':
+                    $btn['class'] .= ' button-nthu-ee';
+                    $btn['innerclass'] .= ' button-inner';
+                    break;
+                default:
+                    break;
+            }
+
+            return $btn;
+        }, $btns);
+
+        foreach ($btns as $btn) {
+            $this->add_button($btn, 'taskbar');
+        }
+    }
+
+    /**
+     * Add buttons to the "toolbar" container.
+     *
+     * @param array[]     $btns the buttons
+     * @param null|string $skin the skin name
+     */
+    public function add_buttons_toolbar(array $btns, ?string $skin = null): void
+    {
+        $skin = $skin ?? RoundcubeHelper::getBaseSkinName();
+
+        $btns = \array_map(function (array $btn) use ($skin): array {
+            switch ($skin) {
+                case 'classic':
+                    $btn['class'] .= ' button';
+                    break;
+                case 'elastic':
+                    $btn['innerclass'] .= ' inner';
+                    break;
+                case 'larry':
+                    $btn['class'] .= ' button';
+                    break;
+                default:
+                    break;
+            }
+
+            return $btn;
+        }, $btns);
+
+        foreach ($btns as $btn) {
+            $this->add_button($btn, 'toolbar');
+        }
     }
 }
